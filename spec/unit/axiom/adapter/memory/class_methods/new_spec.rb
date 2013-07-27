b6462733a5e6c35a8754cc5dcf85de55a0b05e80
Dan Kubb <dan.kubb@gmail.com>
2013-07-27 00:39:29 -0700

    Kill mutation in constructor

--- spec/unit/axiom/adapter/memory/class_methods/new_spec.rb
+++ spec/unit/axiom/adapter/memory/class_methods/new_spec.rb
@@ -9,11 +9,25 @@ describe Adapter::Memory, '.new' do
     let(:schema) { {} }
 
     it { should be_instance_of(described_class) }
+
+    it 'creates a new thread safe hash for the schema' do
+      klass = double('ThreadSafe::Hash')
+      expect(klass).to receive(:new).and_return({})
+      stub_const('ThreadSafe::Hash', klass)
+      subject
+    end
   end
 
   context 'without a schema' do
     subject { described_class.new }
 
     it { should be_instance_of(described_class) }
+
+    it 'creates a new thread safe hash for the schema' do
+      klass = double('ThreadSafe::Hash')
+      expect(klass).to receive(:new).and_return({})
+      stub_const('ThreadSafe::Hash', klass)
+      subject
+    end
   end
 end
